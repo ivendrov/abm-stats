cda6e2b6a845a99144214813543b29802e2bd1e6
Ivan Vendrov <isv452@mail.usask.ca>
2014-06-11 17:58:37 -0600

    added multi-dimensional arrays

---
+++ src/main/java/ca/usask/abm/MultiDimArray.java
@@ -0,0 +1,89 @@
+package ca.usask.abm;
+
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * A class representing multi-dimensional arrays, and allowing
+ * them to be viewed as one-dimensional arrays (using lexicographic order of coordinates)
+ * @author isv452
+ *
+ */
+public class MultiDimArray<T> extends AbstractList<T>{
+	private ArrayList<T> array;
+	private Collection<Integer> dims;
+	
+	/**
+	 * Creates a new array given a sequence of dimensions
+	 * @param ds a nonempty sequence of positive integers
+	 */
+	public MultiDimArray(Collection<Integer> ds){
+		dims = ds;
+		int totalLength = 1;
+		for (int d : dims){
+			totalLength *= d;
+		}
+		array = new ArrayList<T>(totalLength);
+	}
+	
+	/**
+	 * @param index coordinate vector
+	 * @return true if this is a valid coordinate index
+	 */
+	public boolean valid(Collection<Integer> index){
+		if (index.size() != dims.size()) return false;
+		Iterator<Integer> ds = dims.iterator();
+		for (int i : index){
+			if (i < 0 || i >= ds.next()) return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * @precond valid(index)
+	 * @param index a valid coordinate index
+	 * @return the corresponding linear index
+	 */
+	public int toLinearIndex(Collection<Integer> index){
+		int sum = 0;
+		int product = size();
+		Iterator<Integer> ds = dims.iterator();
+		for (int i : index){
+			// update product to be the number of elements in each subarray
+			product /= ds.next();
+			// update sum
+			sum += product * i;
+		}
+		return sum;
+	}
+	
+	/**
+	 * @precond 0 <= index < size()
+	 * @param index a given linear index
+	 * @return the corresponding co-ordinate index
+	 * @postcond toLinearIndex(fromLinearIndex(index)) == index
+	 */
+	public Collection<Integer> fromLinearIndex(int index){
+		ArrayList<Integer> coord = new ArrayList<Integer>(dims.size());
+		int product = size();
+		for (int dim : dims){
+			product /= dim;
+			coord.add(index / product);
+			index %= product;
+		}
+		return coord;
+	}
+	
+	// list methods
+	@Override
+	public int size(){ return array.size();}
+	@Override
+	public T get(int idx){ return array.get(idx);}
+	@Override
+	public T set(int index, T value) { return array.set(index,  value);}
+
+	
+}

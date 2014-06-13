dd8bccc898c6906186d83618c87911c51dbf89bc
Ivan Vendrov <isv452@mail.usask.ca>
2014-06-13 17:52:40 -0600

    added range partitions

--- src/main/java/ca/usask/abm/Partitions.java
+++ src/main/java/ca/usask/abm/Partitions.java
@@ -37,4 +37,32 @@ public class Partitions {
 			public String idLabel(int id) { return String.format("%d", id + min);}
 		};
 	}
+
+	/**
+	 * Partitions an interval on the real line into a set of half-open intervals of given step size
+	 */
+	 public static Partition<Double> range(final double min, final double step, final double max) { 
+		 return new Partition<Double>() {
+		    private Double eps = 1e-8;
+		    @Override
+		    public int toID(Double x){
+		      if (x > max || x < min) return Partition.INVALID_ID;
+		      else { 
+		        double basic = (x - min) / step; // the number of steps needed to get there
+		        int rounded = (int) Math.floor(basic - eps); // the interval number
+		        return Math.max(0, rounded); // make sure the answer is nonnegative
+		      }
+		    }  
+		    public int maxID() { return toID(max);}
+		    
+		    public String idLabel(int id) {
+		        double lowerBound = min + step * id;
+		        double upperBound = lowerBound + step;
+		        if (id == maxID())
+		        	return String.format("[%f, %f]", lowerBound, upperBound);
+		        else 
+		        	return String.format("[%f, %f)", lowerBound, upperBound);
+		    }    
+		 };
+	 }
 }

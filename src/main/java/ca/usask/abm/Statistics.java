6e78a3eb05bb0e77e909e2b6ca6075d276a099a5
Ivan Vendrov <ivendrov@gmail.com>
2014-06-14 20:14:06 -0600

    added documentation on statistics

--- src/main/java/ca/usask/abm/Statistics.java
+++ src/main/java/ca/usask/abm/Statistics.java
@@ -10,6 +10,9 @@ import java.util.Collection;
  *
  */
 public class Statistics {
+   /**
+    * @return a statistic that simply counts the number of elements in the collection
+    */
    public static <T> Statistic<T> count() { 
 	   return new Statistic<T>(){
 		   @Override
@@ -17,16 +20,30 @@ public class Statistics {
 	   };
    }
    
-   public static Statistic<Double> average = new Statistic<Double>() {
+   /**
+    * a statistic that computes the mean of all elements in the collection,
+    * or returns 0 if the collection is empty.
+    */
+   public static Statistic<Double> mean = new Statistic<Double>() {
 		@Override
 		public double collect(Collection<Double> data){
 			double sum = 0;
 			int count = 0;
 			for (double d : data) { sum += d; count++; }
+			if (count == 0) return 0;
 			return sum / count;
 		}
 	};
 	
+	/**
+	 * Creates a statistic over values of type T by applying an accessor to extract a
+	 * values of type U, then applying a statistic over U on the result.
+	 * @param <U> the type of the existing statistic
+	 * @param <T> the type of the resulting statistic
+	 * @param stat the existing statistic
+	 * @param accessor the accessor function
+	 * @return the resulting statistic
+	 */
 	public static <T, U> Statistic<T> lift(final Statistic<U> stat, final Function<T,U> accessor){ 
 		return new Statistic<T>(){
 			@Override

e16f8cd00aa9052252b1300ccf6e64e6665ea975
Ivan Vendrov <ivendrov@gmail.com>
2014-06-16 14:56:49 -0600

    added function static fields in Timed

--- src/main/java/ca/usask/abm/Timed.java
+++ src/main/java/ca/usask/abm/Timed.java
@@ -17,4 +17,32 @@ public class Timed<T> {
 	
 	public T getValue(){ return value;}
 	public double getTime() { return time;}
+	
+	/**
+	 * @return a function that extracts the value from a timed object
+	 */
+	public static <T> Function<Timed<T>, T> valueFunction() {
+		return new Function<Timed<T>, T>(){
+
+			@Override
+			public T apply(Timed<T> argument) {
+				return argument.getValue();
+			}
+			
+		};
+	}
+	
+	/**
+	 * @return a function that extracts the time from a timed object
+	 */
+	public static <T> Function<Timed<T>, Double> timeFunction() {
+		return new Function<Timed<T>, Double>(){
+
+			@Override
+			public Double apply(Timed<T> argument) {
+				return argument.getTime();
+			}
+			
+		};
+	}
 }
